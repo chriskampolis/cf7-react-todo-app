5589690c68c0cdbdfdee3790a600f6a2f1e57649
Chris Kampolis <chris.kampolis@outlook.com>
2025-05-29 17:14:34 +0300

    feat: add toggle completed task functionality

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import { Trash2, Edit, Save, X } from "lucide-react";
+import {Trash2, Edit, Save, X, Square, CheckSquare} from "lucide-react";
 import type { TodoListProps } from "../types.ts";
 import {useState} from "react";
 
@@ -25,12 +25,17 @@ const TodoList = ({todos, dispatch}: TodoListProps) => {
     setEditId(null);
     setEditText("");
   }
-
+  const handleToggle = (id: number) => () => {
+    dispatch({type: "COMPLETE", payload: id})
+  }
   return(
     <>
       <ul className="space-y-2">
         {todos.map(todo => (
-            <li key={todo.id} className="flex items-center justify-between bg-gray-100 p-2 rounded">
+            <li key={todo.id}
+                className={`flex items-center justify-between bg-gray-100 p-2 rounded"
+                  ${todo.completed ? "opacity-60 line-through" : ""} `}
+            >
               { editId === todo.id ?(
                   <>
                     <div className="flex flex-1 gap-2">
@@ -58,7 +63,21 @@ const TodoList = ({todos, dispatch}: TodoListProps) => {
                   </>
                 ): (
                   <>
-                    <span>{todo.text}</span>
+                    <div className="flex items-center gap-2 flex-1">
+                      <button
+                        className="text-green-500"
+                        onClick={handleToggle(todo.id)}
+                      >
+                        {
+                          todo.completed ? (
+                            <CheckSquare size={18} />
+                          ): (
+                            <Square size={18} />
+                          )}
+                      </button>
+                      <span>{todo.text}</span>
+                    </div>
+
                     <div className="flex gap-2">
                       <button
                         onClick={handleEdit(todo.id, todo.text)}
